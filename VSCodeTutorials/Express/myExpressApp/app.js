951a2b6d92cee2ab76e934db579f0b7cf64bb5a1
Jaakko Mattila <jaakko.m.mattila@aalto.fi>
2019-05-20 17:23:37 +0300

    Adding VSCode tutorials folder.

---
+++ VSCodeTutorials/Express/myExpressApp/app.js
@@ -0,0 +1,46 @@
+// These seem to be the dependencies, kind of like using in C#
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+// This seems to call Express constructor to create an application handle 
+var app = express();
+
+// Seems to basically set fields and assign values to them
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// Assigns resources to the object, some seem to attach methods or functionalities to it
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// Likely linking this object with the execution code?
+module.exports = app;
---
+++ VSCodeTutorials/Hello/app.js
@@ -0,0 +1,4 @@
+var msg = 'Hello World';
+console.log(msg);
+console.log("Message length: " + msg.length)
+console.log("Substring of message: " + msg.substr(0,5))
